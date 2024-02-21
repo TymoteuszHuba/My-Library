2d0509d8e8d7e97e7789b79924f829dabeea2e9d
Tymoteusz Huba <tymoteuszhuba@MacBook-Air-Tymoteusz.local>
2024-02-21 19:00:24 +0100

    updated library.js file, the changes include refactor code into two class Book and Library

--- library.js
+++ library.js
@@ -1,14 +1,3 @@
-const myLibrary = [];
-
-const titleInput = document.querySelector('.book-title');
-const authorInput = document.querySelector('.book-author');
-const pagesInput = document.querySelector('.book-pages');
-const statusInput = document.querySelector('.book-status');
-const submitBtn = document.querySelector('.submit');
-
-const form = document.querySelector('.book-form');
-const libraryList = document.querySelector('.library-list');
-
 class Book {
 	constructor(title, author, pages, status) {
 		this.title = title;
@@ -18,76 +7,93 @@ class Book {
 	}
 }
 
-function addBookToLibrary() {
-	if (titleInput.value.length === 0 || authorInput.value.length === 0) {
-		alert('Please add title or author book!');
-		return;
+class Library {
+	constructor() {
+		this.myLibrary = [];
+		this.titleInput = document.querySelector('.book-title');
+		this.authorInput = document.querySelector('.book-author');
+		this.pagesInput = document.querySelector('.book-pages');
+		this.statusInput = document.querySelector('.book-status');
+		this.submitBtn = document.querySelector('.submit');
+
+		this.form = document.querySelector('.book-form');
+		this.libraryList = document.querySelector('.library-list');
+
+		this.submitBtn.addEventListener('click', (event) => {
+			event.preventDefault();
+			this.addBookToLibrary();
+			this.showBook();
+			this.clearInputs();
+		});
+
+		this.libraryList.addEventListener('click', (event) => {
+			const index = event.target.closest('.library-item').dataset.index;
+			if (event.target.classList.contains('remove-btn')) {
+				this.removeBook(index);
+				return;
+			}
+
+			if (event.target.classList.contains('read-btn')) {
+				this.statusToggle(index);
+				return;
+			}
+		});
 	}
 
-	if (pagesInput.value === String || pagesInput.value.length === 0) {
-		alert('Please set a page numbers!');
-		return;
+	addBookToLibrary() {
+		if (this.titleInput.value.length === 0 || this.authorInput.value.length === 0) {
+			alert('Please add title or author book!');
+			return;
+		}
+
+		if (this.pagesInput.value === String || this.pagesInput.value.length === 0) {
+			alert('Please set a page numbers!');
+			return;
+		}
+
+		const book = new Book(
+			this.titleInput.value,
+			this.authorInput.value,
+			this.pagesInput.value,
+			this.statusInput.value
+		);
+
+		this.myLibrary.push(book);
+		console.log(this.myLibrary);
 	}
 
-	const book = new Book(
-		titleInput.value,
-		authorInput.value,
-		pagesInput.value,
-		statusInput.value
-	);
+	showBook() {
+		this.libraryList.innerHTML = '';
+		this.myLibrary.forEach((book, index) => {
+			const bookItem = document.createElement('div');
+			bookItem.classList.add('library-item');
+			bookItem.setAttribute('data-index', index);
+			bookItem.innerHTML = `<p>${book.title}</p>
+			<p>${book.author}</p>
+			<p>${book.pages}</p>
+			<button class="library-item-btn read-btn">${book.status}</button>
+			<button class="library-item-btn remove-btn">x</button>`;
 
-	myLibrary.push(book);
-	console.log(myLibrary);
-}
-
-function showBook() {
-	libraryList.innerHTML = '';
-	myLibrary.forEach((book, index) => {
-		const bookItem = document.createElement('div');
-		bookItem.classList.add('library-item');
-		bookItem.setAttribute('data-index', index);
-		bookItem.innerHTML = `<p>${book.title}</p>
-        <p>${book.author}</p>
-        <p>${book.pages}</p>
-        <button class="library-item-btn read-btn">${book.status}</button>
-        <button class="library-item-btn remove-btn">x</button>`;
-
-		libraryList.appendChild(bookItem);
-	});
-}
-
-function removeBook(index) {
-	myLibrary.splice(index, 1);
-	showBook();
-}
-
-function statusToggle(index) {
-    myLibrary[index].status = myLibrary[index].status === 'Read' ? 'Unread' : 'Read';
-    showBook();
-}
-
-function clearInputs() {
-	titleInput.value = '';
-	authorInput.value = '';
-	pagesInput.value = '';
-}
-
-submitBtn.addEventListener('click', (event) => {
-	event.preventDefault();
-	addBookToLibrary();
-	showBook();
-	clearInputs();
-});
-
-libraryList.addEventListener('click', (event) => {
-	const index = event.target.closest('.library-item').dataset.index;
-	if (event.target.classList.contains('remove-btn')) {
-		removeBook(index);
-		return;
+			this.libraryList.appendChild(bookItem);
+		});
 	}
 
-	if (event.target.classList.contains('read-btn')) {
-		statusToggle(index);
-		return;
+	removeBook(index) {
+		this.myLibrary.splice(index, 1);
+		this.showBook();
 	}
-});
+
+	statusToggle(index) {
+		this.myLibrary[index].status =
+			this.myLibrary[index].status === 'Read' ? 'Unread' : 'Read';
+		this.showBook();
+	}
+
+	clearInputs() {
+		this.titleInput.value = '';
+		this.authorInput.value = '';
+		this.pagesInput.value = '';
+	}
+}
+
+const library = new Library();
